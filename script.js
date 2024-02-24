f8260a6779a19607a1b8776b536a6764727cfeec
Jescel James <jesceljames@gmail.com>
2024-02-24 13:54:19 -0500

    new div and new function added for forecast

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const cityFormEl = document.querySelector("#city-form");
 const cityInputEl = document.querySelector("#city-input");
 const weatherContainerEl = document.querySelector("#weather-container");
+const forecastContainerEl = document.querySelector('#forecast-container');
 const citySearchTermEl = document.querySelector("#city-searched");
 const historyContainerEl = document.querySelector("#history-container");
 
@@ -22,10 +23,9 @@ console.log(window);
 
     const city = cityInputEl.value.trim();
 
-    
-
     if (city) {
-      getWeather(city); // argument aka "baseball" passes (city) to getWeather fucntion "glove"
+      // getWeather(city); // argument aka "baseball" passes (city) to getWeather function "glove"
+      getForecastWeather(city);
       addCityButton(city);
             
       weatherContainerEl.textContent = "";
@@ -36,14 +36,11 @@ console.log(window);
   };
 
 
- // API
+ // API TODAY'S WEATHER
   const getWeather = function (city) {
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     citySearchTermEl.textContent = city.toUpperCase();
-
-     
-    
 
     fetch(weatherApi)
       .then(function (response) {
@@ -63,8 +60,6 @@ console.log(window);
         const cityWind = document.createElement("p");
         const cityHumidity = document.createElement("p");
 
-
-
         // BUILD ____________________________________________
         const unixDate = weatherData.dt; // unixDate api data date is in unix
         const date = new Date(unixDate * 1000); // convert date
@@ -82,8 +77,6 @@ console.log(window);
         cityWind.textContent = `Wind: ${weatherData.wind.speed} MPH`;
         cityHumidity.textContent = `Humidity: ${weatherData.main.humidity} %`;
 
-
-
         // PLACE ___________________________________________
         weatherContainerEl.appendChild(cityDate);
         weatherContainerEl.appendChild(cityName);
@@ -93,9 +86,74 @@ console.log(window);
         weatherContainerEl.appendChild(cityWind);
         weatherContainerEl.appendChild(cityHumidity);
 
-
       });
   };
+
+
+
+
+
+
+
+
+
+
+
+// API FORECAST WEATHER
+const getForecastWeather = function (city) {
+  const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
+
+  citySearchTermEl.textContent = city.toUpperCase();
+
+  fetch(weatherApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      const weatherData = data;
+
+      // CREATE _______________________________________
+      // const cityDate = document.createElement("h5");
+      const cityName = document.createElement("h6");
+      // const weatherMain = document.createElement("p");
+      // const weatherIcon = document.createElement("img");
+      // const cityTemp = document.createElement("p");
+      // const cityWind = document.createElement("p");
+      // const cityHumidity = document.createElement("p");
+
+      // BUILD ____________________________________________
+      // const unixDate = weatherData.dt; // unixDate api data date is in unix
+      // const date = new Date(unixDate * 1000); // convert date
+      // cityDate.textContent = date.toLocaleDateString(); // binds to local date string
+
+      cityName.textContent = weatherData.city.name;
+      // weatherMain.textContent = weatherData.weather[0].main;
+      // // Weather Icon
+      // const iconCode = weatherData.weather[0].icon; // https://openweathermap.org/weather-conditions#Icon-list
+      // const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+      // weatherIcon.src = iconUrl;
+      // weatherIcon.classList.add("weather-icon"); //makes icon smaller
+
+      // cityTemp.textContent = `Temp: ${weatherData.main.temp} F`;
+      // cityWind.textContent = `Wind: ${weatherData.wind.speed} MPH`;
+      // cityHumidity.textContent = `Humidity: ${weatherData.main.humidity} %`;
+
+      // PLACE ___________________________________________
+      // weatherContainerEl.appendChild(cityDate);
+      forecastContainerEl.appendChild(cityName);
+      // weatherContainerEl.appendChild(weatherMain);
+      // weatherContainerEl.appendChild(weatherIcon);
+      // weatherContainerEl.appendChild(cityTemp);
+      // weatherContainerEl.appendChild(cityWind);
+      // weatherContainerEl.appendChild(cityHumidity);
+
+    });
+};
+
+
+
 
 
  // Searched History
